bba8f772fcb8c5962756071d25115d94a3e01002
Rohit Yarra <yarrarohit250@gmai.com>
2023-03-07 13:59:11 +0530

    first commit

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const sensorDataRoutes = require('./routes/sensordataRoutes');
+const expDataRoutes = require("./routes/experimentdataRoutes");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", sensorDataRoutes);
+app.use("/exp", expDataRoutes);
+
+const port=process.env.PORT || 5006
+
 mongoose.set('strictQuery', false);
 
 mongoose
@@ -16,9 +21,8 @@ mongoose
         'mongodb+srv://firesafety:123aes456@cluster0.gxvnnwa.mongodb.net/firesafety?retryWrites=true&w=majority'
     )
     .then((result) => {
-
         console.log("app is running...");
-        app.listen(5006);
+        app.listen(port);
     }).catch((err)=>{
         console.log(err);
     });
